4fc423c314b9882c00c0644d597ce287dbce78e7
ritesh9661 <kritesh5march@gmail.com>
2023-09-30 00:20:35 +0530

    version 3.0.2

--- Backend/server.js
+++ Backend/server.js
@@ -1,31 +1,27 @@
-const express = require('express')
-const mongoose =require('mongoose')
- 
-const app=express()
-// const url = `mongodb+srv://${dbData.username}:${dbData.pass}@cluster0.sxavqzw.mongodb.net/${dbData.dbName}?retryWrites=true&w=majority`;
-mongoose.connect("mongodb+srv://kritesh5march:Ritesh123@cluster0.3obfhzu.mongodb.net/crud")
+const express = require("express");
+const mongoose = require("mongoose");
 
-const UserSchema=new mongoose.Schema({
-    Project_Title:String
+const app = express();
+mongoose.connect(
+  "mongodb+srv://kritesh5march:Ritesh123@cluster0.3obfhzu.mongodb.net/crud"
+);
 
-})
-const UserModel=mongoose.model("users", UserSchema)
+const UserSchema = new mongoose.Schema({
+  Project_Title: String,
+});
+const UserModel = mongoose.model("users", UserSchema);
 
-app.get("/get",(req,res)=>{
-    res.json({"Ritesh":["Kumar","uhhsanj"]})
-})
-
-
-app.get("/api",(req,res)=>{
-    UserModel.find({}).then(function(users){
-        res.json(users)
-        console.log("Connected to mongodb");
-    }).catch(function(err){
-        console.log(err);
+app.get("/api", (req, res) => {
+  UserModel.find({})
+    .then(function (users) {
+      res.json(users);
+      console.log("Connected to mongodb");
     })
-})
+    .catch(function (err) {
+      console.log(err);
+    });
+});
 
-
-app.listen(5000,()=>{
-    console.log("server is runing on 5000");
-})
+app.listen(5000, () => {
+  console.log("server is runing on 5000");
+});
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -35,53 +35,52 @@ function App() {
 
   return (
     <div className="container mx-auto ">
-              {isClicked && (
-          <div
-            className="fixed right-0 flex flex-col cursor-not-allowed justify-between popUp py-5 sm:h-full h-3/4 sm:w-[30%]  w-full align-bottom profile-popup text-sm"
-            onClick={(e) => togleClicked()}
-          > 
-            
-            <div className=" font-semibold m-3 text-2xl">{clickData.Project_Title}</div>
-            <div className="m-3 ">
-              <div className=" font-semibold flex max-w-max align-middle text-xl">
-                <img className=" pe-2 h-5 m-auto" src={icon} /> Title
-              </div>
-              <div className=" text-lg">{clickData.Project_Title}</div>
+      {isClicked && (
+        <div
+          className="fixed right-0 flex flex-col cursor-not-allowed justify-between popUp py-5 sm:h-full h-3/4 sm:w-[30%]  w-full align-bottom profile-popup text-sm"
+          onClick={(e) => togleClicked()}
+        >
+          <div className=" font-semibold m-3 text-2xl">
+            {clickData.Project_Title}
+          </div>
+          <div className="m-3 ">
+            <div className=" font-semibold flex max-w-max align-middle text-xl">
+              <img className=" pe-2 h-5 m-auto" src={icon} /> Title
             </div>
-            <div className="m-3 text-lg">
-              <div className=" font-semibold flex max-w-max align-middle text-xl">
-                <img className=" pe-2 h-5 m-auto" src={icon} />
-                Project.Technologies
-              </div>
-              <div className=" max-w-full">
-                {clickData.Project_Technologies}
-              </div>
+            <div className=" text-lg">{clickData.Project_Title}</div>
+          </div>
+          <div className="m-3 text-lg">
+            <div className=" font-semibold flex max-w-max align-middle text-xl">
+              <img className=" pe-2 h-5 m-auto" src={icon} />
+              Project.Technologies
             </div>
-            <div className="m-3 text-lg">
-              <div className=" font-semibold flex max-w-max align-middle text-xl">
-                <img className=" pe-2 h-5 m-auto" src={icon} />
-                Technical_Skillset.Frontend
-              </div>
-              <div>{clickData.Technical_Skillset_Frontend}</div>
+            <div className=" max-w-full">{clickData.Project_Technologies}</div>
+          </div>
+          <div className="m-3 text-lg">
+            <div className=" font-semibold flex max-w-max align-middle text-xl">
+              <img className=" pe-2 h-5 m-auto" src={icon} />
+              Technical_Skillset.Frontend
             </div>
-            <div className="m-3 text-lg">
-              <div className=" font-semibold flex max-w-max align-middle text-xl">
-                <img className=" pe-2 h-5 m-auto" src={icon} />
-                Technical_Skillset.Backend
-              </div>
-              <div>{clickData.Technical_Skillset_Backend}</div>
+            <div>{clickData.Technical_Skillset_Frontend}</div>
+          </div>
+          <div className="m-3 text-lg">
+            <div className=" font-semibold flex max-w-max align-middle text-xl">
+              <img className=" pe-2 h-5 m-auto" src={icon} />
+              Technical_Skillset.Backend
             </div>
-            <div className="m-3 text-lg">
-              <div className=" font-semibold flex max-w-max align-middle text-xl">
-                <img className=" pe-2 h-5 m-auto" src={icon} />
-                Other_Information_Availability
-              </div>
-              <div className=" overflow-auto">
-                {clickData.Other_Information_Availability}
-              </div>
+            <div>{clickData.Technical_Skillset_Backend}</div>
+          </div>
+          <div className="m-3 text-lg">
+            <div className=" font-semibold flex max-w-max align-middle text-xl">
+              <img className=" pe-2 h-5 m-auto" src={icon} />
+              Other_Information_Availability
+            </div>
+            <div className=" overflow-auto">
+              {clickData.Other_Information_Availability}
             </div>
           </div>
-        )}
+        </div>
+      )}
       <div className=" text-center">
         <input
           ref={inputRef}
@@ -105,42 +104,46 @@ function App() {
                   val.Project_Technologies.toLowerCase().includes(search);
           }).map((val) => {
             return (
-              
               <div className="  card p-2 rounded">
-                <div
-                  key={val._id}
-                  className=" p-2 "
-                  onClick={(e) => view(val)}
-                >
+                <div key={val._id} className=" p-2 " onClick={(e) => view(val)}>
                   <div className="py-2">
                     <div className=" text-slate-400 text-sm">Title</div>
                     <div className=" font-semibold">{val.Project_Title}</div>
                   </div>
                   <div className="py-2">
-                    <div className=" text-slate-400 text-sm">Project.Technologies</div>
-                    <div><ReadMore>{val.Project_Technologies}</ReadMore></div>
+                    <div className=" text-slate-400 text-sm">
+                      Project.Technologies
+                    </div>
+                    <div>
+                      <ReadMore>{val.Project_Technologies}</ReadMore>
+                    </div>
                   </div>
                   <div className=" py-2">
                     <div className=" text-slate-400 text-sm">
                       Technical_Skillset.Frontend
                     </div>
-                    <div><ReadMore>{val.Technical_Skillset_Frontend}</ReadMore></div>
+                    <div>
+                      <ReadMore>{val.Technical_Skillset_Frontend}</ReadMore>
+                    </div>
                   </div>
                   <div className="py-2">
                     <div className=" text-slate-400 text-sm">
                       Technical_Skillset.Frontend
                     </div>
-                    <div><ReadMore>{val.Technical_Skillset_Backend}</ReadMore></div>
+                    <div>
+                      <ReadMore>{val.Technical_Skillset_Backend}</ReadMore>
+                    </div>
                   </div>
                   <div className="py-2">
                     <div className=" text-slate-400 text-sm">
                       Other_Information_Availability
                     </div>
-                    <div><ReadMore>{val.Other_Information_Availability}</ReadMore></div>
+                    <div>
+                      <ReadMore>{val.Other_Information_Availability}</ReadMore>
+                    </div>
                   </div>
                 </div>
               </div>
-            
             );
           })}
       </div>
